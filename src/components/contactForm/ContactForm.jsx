387e0c766d0ca8d474aaf6fed613c49682898450
wassim <63935826+wassimelboussi@users.noreply.github.com>
2024-03-07 00:19:29 +0400

    add margin to contact me input fields for small screens

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -27,37 +27,37 @@ export default function ContactForm() {
         <h2 className="text-4xl mb-4 text-green-300 font-bold text-center md:text-5xl">Contact Me</h2>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <div className="mb-4">
-            <label htmlFor="name" className="block text-white">Name</label>
+            <label htmlFor="name" className="block text-white mx-4 md:m-0 lg:m-0">Name</label>
             <input
               type="text"
               id="name"
               name="name"
               value={formData.name}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              className="w-[90%] md:w-full lg:w-full mx-4 md:mx-0 lg:mx-0 px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-white">Email Address</label>
+            <label htmlFor="email" className="block text-white mx-4 md:m-0 lg:m-0">Email Address</label>
             <input
               type="email"
               id="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              className="w-[90%] md:w-full lg:w-full mx-4 md:mx-0 lg:mx-0 px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="message" className="block text-white">Message</label>
+            <label htmlFor="message" className="block text-white mx-4 md:m-0 lg:m-0">Message</label>
             <textarea
               id="message"
               name="message"
               value={formData.message}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg resize-none focus:outline-none focus:border-blue-500"
+              className="w-[90%] md:w-full lg:w-full mx-4 md:mx-0 lg:mx-0 px-4 py-2 border rounded-lg resize-none focus:outline-none focus:border-blue-500"
               rows="4"
               required
             />
