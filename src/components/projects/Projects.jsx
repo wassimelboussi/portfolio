c62fe2fe8585033a0268871cdc32e829bdbb08cc
wassim <63935826+wassimelboussi@users.noreply.github.com>
2024-03-07 04:08:37 +0400

    update public folder

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,7 +8,7 @@ export default function Projects() {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <ProjectCard
-            imageSrc="src/images/project-images/CBA.png"
+            imageSrc="/images/project-images/CBA.png"
             title="Classroom Behavior Analyzer"
             description="This is a tool that helps educators analyze and track the focus level of their students during class. It uses facial recognition to detect the students' faces and then uses a machine learning model to determine whether the student is focused or not. This project was built using Python, OpenCV, and Tensorflow."
             sourceCodeLink="project1-source-code-link"
@@ -19,7 +19,7 @@ export default function Projects() {
             }}
           />
           <ProjectCard
-            imageSrc="src/images/project-images/vue-weather.png"
+            imageSrc="/images/project-images/vue-weather.png"
             title="Vue Weather App"
             description="I developed a weather app using Vue JS, incorporating dynamic features for real-time updates. The app seamlessly integrates with weather APIs, providing users with accurate forecasts. Its responsive design ensures a smooth experience, making it a user-friendly and visually appealing tool for tracking weather conditions."
             sourceCodeLink="project2-source-code-link"
@@ -30,7 +30,7 @@ export default function Projects() {
             }}
           />
           <ProjectCard
-            imageSrc="src/images/project-images/myportfolio.png"
+            imageSrc="/images/project-images/myportfolio.png"
             title="My Portfolio"
             description="Featured in the projects section of my portfolio is the portfolio itself. Developed with ReactJS and styled with Tailwind CSS, this project allows viewers to access its source code."
             sourceCodeLink="project3-source-code-link"
